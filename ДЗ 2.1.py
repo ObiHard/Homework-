5894d810a0e53da0134e8feea169d8f4f487310f
ob1 <dute000777@gmail.com>
2025-06-17 17:49:20 +0300

    Create homework 2.1

---
+++ ДЗ 2.1.py
@@ -0,0 +1,13 @@
+number = int(input("Введіть 4-значне число : "))
+
+one = number // 1000
+two = (number % 1000) // 100
+three = (number % 100) // 10
+four = number % 10
+
+print(one)
+print(two)
+print(three)
+print(four)
+print("Дякую за увагу ! ! !")
+print("*_*")
